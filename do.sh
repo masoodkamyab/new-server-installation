d400e45296ae3cd0b727c67441d849b019567e87
Masood Ubuntu 24.04 <masoodkazemii94@gmail.com>
2024-12-12 13:56:26 +0330

    Update Non-interactive installation of iptables-persistent

--- do.sh
+++ do.sh
@@ -84,6 +84,12 @@ echo "Starting system preparation..."
 apt update
 apt -y dist-upgrade
 apt -y purge ufw
+
+
+# Pre-answer iptables-persistent prompts
+export DEBIAN_FRONTEND=noninteractive
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt -y install iptables-persistent vim
 
 
